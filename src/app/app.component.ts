3d319cd5f95d43de33635d085b50a54e73b18f80
Stefano Paulo <stefanopaulo47@gmail.com>
2021-07-04 10:05:35 -0300

    lendo os itens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,8 @@ export class AppComponent {
   public title: string = 'Minhas Tarefas';
 
   constructor(private formBuilder: FormBuilder) {
+    this.load();
+
     this.form = this.formBuilder.group({
       title: ['', Validators.compose([
         Validators.minLength(3),
@@ -26,6 +28,7 @@ export class AppComponent {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, title, false));
+    this.save();
     this.clear();
   }
 
@@ -39,13 +42,27 @@ export class AppComponent {
     if (index !== -1) {
       this.todos.splice(index, 1);
     }
+
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load() {
+    const data = localStorage.getItem('todos');
+    this.todos = JSON.parse(data);
   }
 }
