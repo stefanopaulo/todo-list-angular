d135a5476b46c534b0ef8fd3d2e718dd93d54d4c
Stefano Paulo <stefanopaulo47@gmail.com>
2021-07-04 09:36:54 -0300

    forms basico

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -7,10 +8,19 @@ import { Todo } from 'src/models/todo.model';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public form: FormGroup;
   public todos: Todo[] = [];
   public title: string = 'Minhas Tarefas';
 
-  constructor() {
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required
+      ])]
+    });
+
     this.todos.push(new Todo(1, 'passear com o cachorro', false));
     this.todos.push(new Todo(2, 'ir ao mercado', false));
     this.todos.push(new Todo(3, 'cortar o cabelo', true));
