0b275c9088fc5812d93bf81079c90623cc58be07
Stefano Paulo <stefanopaulo47@gmail.com>
2021-07-04 09:56:26 -0300

    adicionando uma tarefa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,10 +20,17 @@ export class AppComponent {
         Validators.required
       ])]
     });
+  }
 
-    this.todos.push(new Todo(1, 'passear com o cachorro', false));
-    this.todos.push(new Todo(2, 'ir ao mercado', false));
-    this.todos.push(new Todo(3, 'cortar o cabelo', true));
+  add() {
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.clear();
+  }
+
+  clear() {
+    this.form.reset();
   }
 
   remove(todo: Todo) {
